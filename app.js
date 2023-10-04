2394602ce375dfe28bb884161a683e6b7cd524d9
Niyad <niyadpunniyath007@gmail.com>
2023-10-04 15:19:31 +0530

    user select disable

--- app.js
+++ app.js
@@ -27,3 +27,42 @@ arr.forEach(button => {
 
     })
 })
+
+const ages = ["a", "ab", "ac", "dc" ];
+
+const myArr = [1, [2, 3], 4,[5,[6]]];
+
+
+
+console.log(
+    ages.filter((it) => it != "a",
+    )
+    ,
+    myArr.flatMap(v => v)
+);
+
+
+const q1 = ["Jan", "Feb", "Mar"];
+const q2 = ["Apr", "May", "Jun"];
+const q3 = ["Jul", "Aug", "Sep"];
+const q4 = ["Oct", "Nov", "May"];
+
+const year = [...q1, ...q2, ...q3, ...q4];
+
+console.log({
+    year
+});
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const nums = [4,8,6,4,65,548,4154,54,4];
+
+console.log(fruits,nums);
+console.log(nums.reverse());
+
+fruits.sort();
+console.log(fruits);
+nums.sort((a,b) => b - a)
+console.log(nums);
+
+
+
